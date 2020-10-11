69575c3ebdaf586323600baeba16b292ed57571e
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2020-10-11 20:35:02 +0100

    init

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -12,4 +12,23 @@ object main extends App{
   )
   val jsonFromFields: Json = Json.fromFields(fieldList)
 
+  println(jsonFromFields.noSpaces)
+  println(jsonFromFields.spaces2)
+  println(jsonFromFields.spaces4)
+ /**********************************************************/
+
+ val jsonArray: Json = Json.fromValues(List(
+   Json.fromFields(List(("field1", Json.fromInt(1)))),
+   Json.fromFields(List(
+     ("field1", Json.fromInt(200)),
+     ("field2", Json.fromString("Having circe in Scala Exercises is awesome"))))))
+
+  println(jsonArray)
+
+  def transformJson(jsonArray: Json): Json =
+    jsonArray mapArray { oneJson: Vector[Json] =>
+      oneJson.init
+    }
+
+  println(transformJson(jsonArray).noSpaces)
 }
